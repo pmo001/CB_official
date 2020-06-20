80ea3695812bccf87a61df0a55a5b0a0e0317802
pmo001 <pmo001@ucr.edu>
2020-06-19 22:52:37 -0700

    optimized find second maxVal

--- Educative/second_maxVal.py
+++ Educative/second_maxVal.py
@@ -1,13 +1,17 @@
 def findSecondMaximum(lst):
-    second_maxVal = float("-inf") #imp: must assign -8 or there will be edge cases that won't be fulfilled
+    second_maxVal = float("-inf") #imp: must assign -8 or exist edge cases that won't be fulfilled
     maxVal = float("-inf")
+
     #traversing list twice: O(n)
     for j in range(len(lst)):
         if lst[j] > maxVal:
             maxVal = lst[j]
     
     for i in range(len(lst)):
-        if lst[i] > second_maxVal and lst[i] < maxVal:
+        #less correct: lst[i] < maxVal: or lst[i] != maxVal
+        #below accounts for more edge cases
+            #accounts for duplicate numbers; <is not> makes sure is not the same object as maxVal
+        if lst[i] > second_maxVal and lst[i] is not maxVal:
             second_maxVal = lst[i]
             
     return second_maxVal
