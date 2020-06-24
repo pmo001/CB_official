80b1d7b9671b12aff4896e38869454067c4f61bd
pmo001 <pmo001@ucr.edu>
2020-06-24 03:41:29 -0700

    updated with more helpful comments

--- Educative/left_neg_right_pos.py
+++ Educative/left_neg_right_pos.py
@@ -1,18 +1,30 @@
-#in-place: O(1) space
-def rearrange(lst): # [0 | -2,10]->[-2 | 0, 10] //not last_left
+# [0 | -2,10]->[-2 | 0, 10] //not last_left
 #is last left [-1, -3 | 0]
-#marker for left of delimitation line; keeps track of latest left as adding neg. shifts line forward
-    last_left = 0
+
+#in-place: O(1) space
+def rearrange(lst): 
+    #2nd attempt: 
+    #only able to figure it out with example output; next time, don't depend on example output
+
+    #marker for left of delimitation line; keeps track of latest left as "adding" neg- 
+    # shifts line forward
+    partition_left_idx = 0
     for idx in range(len(lst)):
-        if lst[idx] < 0: #if neg.
-            if idx is not last_left: #else, it would just switch with itself
-                lst[last_left], lst[idx] = lst[idx], lst[last_left] #swap
-            #since swap added neg. to left, shift delimiter line up one
-            last_left += 1
+        if lst[idx] < 0: #if neg-
+            #needs this _if_ to reduce the num of unnec- swaps
+            if idx is not partition_left_idx: #else, it would just switch with itself
+                lst[partition_left_idx], lst[idx] = lst[idx], lst[partition_left_idx] #swap
+            #since swap "added" neg- to left, shift delimiter line up one
+                #line16 is not at this indent cuz 
+                # line needs to move up whether swap happens or not
+            partition_left_idx += 1
     
     return lst
 
 #O(n) time
 print(rearrange([-1, 2, -3, -4, 5]))
+print([-1, -3, -4, 2, 5])
 print(rearrange([300, -1, 3, 0]))
+print([-1, 300, 3, 0])
 print(rearrange([0, 0, 0, -2]))
+print([-2, 0, 0, 0])
